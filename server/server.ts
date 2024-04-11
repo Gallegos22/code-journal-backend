3d18dd83599b39f9a917a118f7c1efa4f6a2f4aa
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-04-11 20:33:52 +0000

    We did post

--- server/server.ts
+++ server/server.ts
@@ -58,6 +58,54 @@ app.get('/api/entries/:entryId', async (req, res, next) => {
   }
 });
 
+app.post('/api/entries', async (req, res, next) => {
+  try {
+    const { title, notes, photoUrl } = req.body;
+    const sql = `
+    insert into "entries" ("title", "notes", "photoUrl")
+    values ($1 ,$2 ,$3 )
+    returning *;`;
+    if (!title || !notes || !photoUrl) {
+      throw new ClientError(400, 'please enter title, notes, & photoUrl');
+    }
+    const params = [title, notes, photoUrl];
+    const result = await db.query<Entry>(sql, params);
+    const [newEntry] = result.rows;
+    res.status(201).json(newEntry);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put('/api/entries/:entryId', async (req, res, next) => {
+  try {
+    const entryId = Number(req.params.entryId);
+    if (!Number.isInteger(entryId) || entryId < 1) {
+      throw new ClientError(400, 'entryId must be a positive integer');
+    }
+    const { title, notes, photoUrl } = req.body;
+    if (!title || !notes || !photoUrl) {
+      throw new ClientError(400, 'please enter title, notes, & photoUrl');
+    }
+    const sql = `
+    update "entries"
+    set "title" = $1,
+        "notes" = $2,
+        "photoUrl" = $3
+      where "entryId" = $4
+      returning *;`;
+    const params = [title, notes, photoUrl, entryId];
+    const result = await db.query<Entry>(sql, params);
+    const [updatedEntry] = result.rows;
+    if (!updatedEntry) {
+      throw new ClientError(404, `cannot find entry with ${entryId}`);
+    }
+    res.json(updatedEntry);
+  } catch (err) {
+    next(err);
+  }
+});
+
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
